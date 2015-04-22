2fb8c5d9adc8b5945bf6aa6c7452631eb280c0ec
vdogamer <ctd8z@virginia.edu>
2015-04-22 14:15:50 -0400

    Adding the form processing tasks

--- process.php
+++ process.php
@@ -10,9 +10,50 @@
 		$number = $_POST['number'];    // number is from the DOM 
 		$selected_choice = $_POST['choice'];     // choice is from the DOM
 
-		//echo $number.'<br>';
-		//echo $selected_choice;
+		// redirect the user to the next question
+		$next = $number++;
 
-		print_r($_POST);
+		/*
+		*	Get total number of questions
+		*/
+
+		$query = "SELECT * FROM `questions`";
+
+		// Get result
+		$results = $mysqli->query($query) or die($mysqli->error.__LINE__);
+		$total = $results->num_rows;
+
+		/*
+		*	Get correct choice
+		*/
+
+		$query = "SELECT * FROM `choices`
+					WHERE question_number = $number AND is_correct = 1";
+
+		// Get result
+		$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+		// Get row
+		$row = $result->fetch_assoc();
+
+		// Set the correct choice
+		$correct_choice = $row['id'];
+
+		// Compare
+		if($correct_choice == $selected_choice){
+
+			// Answer is correct
+			$_SESSION['score']++;
+		}
+
+		// Check to see if this is the last question
+		if($number == $total){
+
+			header("Location: final.php");
+			exit();
+
+		} else {
+			header("Location: question.php?n=".$next)
+		}
 	}
 ?>
